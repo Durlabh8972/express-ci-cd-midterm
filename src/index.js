1033fac70d693a628ebe725f49e0d5b95cfdfb9a
durlabh8972 <dtilavat8972@conestogac.on.ca>
2025-06-19 10:59:27 -0400

    Initial commit

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
+// Routes
 app.get("/", (req, res) => {
   res.send("Hello from CI/CD App!");
 });
@@ -14,8 +15,11 @@ app.use((req, res) => {
   res.status(404).send("Not Found");
 });
 
-app.listen(port, () => {
-  console.log(`App listening at http://localhost:${port}`);
-});
+// Only listen if run directly (not during tests)
+if (require.main === module) {
+  app.listen(port, () => {
+    console.log(`App listening at http://localhost:${port}`);
+  });
+}
 
 module.exports = app;
